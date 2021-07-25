2bdf5b436216931ffdfdb67d0e8de6b0cd074e3f
Jaakko-H <jaheikela@gmail.com>
2021-07-25 10:34:43 +0300

    Commit base project

---
+++ src/test/kotlin/com/jh/tictactoe/TicTacToeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jh.tictactoe
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TicTacToeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
